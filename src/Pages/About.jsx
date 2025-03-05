eb31dda604cde1af8d6d275a52794c7fced16818
kashak2025 <nadodakashak2025.katargam@gmail.com>
2025-03-05 15:27:36 +0530

    last update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,18 +22,10 @@ const About = () => {
             <Box sx={{ fontFamily: '"Outfit",serif', fontSize: "29px" }}>
               About Us
             </Box>
-            <Box>
-              Welcome to{" "}
-              <span style={{ fontFamily: '"Outfit",serif' }}>
-                Skill Swap India
-              </span>
-              , the ultimate platform for skill exchange! We believe that
-              everyone has something valuable to teach and something new to
-              learn. Our mission is to create a community where people can share
-              their skills, learn from others, and grow together—without the
-              need for money. Whether you're a seasoned expert or a curious
-              beginner, this is the place to connect, collaborate, and exchange
-              knowledge.
+            <Box className="content v2">
+              <p>
+                Welcome to Skill Swap India, the ultimate platform for skill exchange! We believe thateveryone has something valuable to teach and something new tolearn. Our mission is to create a community where people can sharetheir skills, learn from others, and grow together—without theneed for money. Whether you're a seasoned expert or a curiousbeginner, this is the place to connect, collaborate, and exchangeknowledge.
+              </p>
             </Box>
           </Box>
         </Container>
@@ -50,7 +42,6 @@ const About = () => {
         </Box>
 
         <Box>
-          <h1>HIGHLIGHT 📝</h1>
           <div className="content v2">
             <p>
               Lorem ipsum dolor sit amet. Qui recusandae itaque et suscipit neque et corrupti beatae aut quas officia quo placeat nobis eos expedita odit est vitae culpa? Et porro consequatur id culpa iusto ut architecto nesciunt.
