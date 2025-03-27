413b285de2f9e34c3138b25ae4a62d6002b9f17d
kashak2025 <nadodakashak2025.katargam@gmail.com>
2025-03-27 13:56:49 +0530

    updated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Footer from '../Components/Footer'
 import Header from '../Components/Header'
-import { Box } from '@mui/material'
+import Img1 from '../images/Contact.jpg'
+import { Box, Container } from '@mui/material'
+import { ContactMail } from '@mui/icons-material'
 const Contact = () => {
   return (
     <div>
-        <Header/>
-        <Box sx={{mt:'90px'}}>
-        Contact US
-      </Box>
-        <Footer/>
+      <Header/>
+        <Box sx={{mt:'74px',position:'relative'}}>
+          <Box>
+            <img src={Img1} width={"100%"} alt="" />
+          </Box>
+          <Box sx={{width:'100%', padding:'100px 0px', position:'absolute', background:'#ffffff94', backdropFilter:'blur("50px") !important', color:'#fff', top:'0px'}}>
+            <Container>
+            <Box>
+              Content
+            </Box>
+           </Container>
+          </Box>
+        </Box>
+      <Footer/>
     </div>
   )
 }
